e53e6bad63574f6687fd36d527305dc1e6306936
helzze <sebastiansancaowo@gmail.com>
2024-10-25 21:02:28 -0500

    proyecto spring pregunta 2

---
+++ src/main/java/daw/t3/p2/sancaydiaz/daw_t3_pregunta2_sancaydiaz/DawT3Pregunta2SancaydiazApplication.java
@@ -0,0 +1,13 @@
+package daw.t3.p2.sancaydiaz.daw_t3_pregunta2_sancaydiaz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DawT3Pregunta2SancaydiazApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DawT3Pregunta2SancaydiazApplication.class, args);
+	}
+
+}
---
+++ src/test/java/daw/t3/p2/sancaydiaz/daw_t3_pregunta2_sancaydiaz/DawT3Pregunta2SancaydiazApplicationTests.java
@@ -0,0 +1,13 @@
+package daw.t3.p2.sancaydiaz.daw_t3_pregunta2_sancaydiaz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DawT3Pregunta2SancaydiazApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
